ec421267bf02af73f3d6e47d9e73c782971ff20b
Arthur Erlendsson <arthur.erlendsson@gmail.com>
2016-10-31 22:22:08 -0400

    create script to test db

---
+++ scripts/testviz_election.py
@@ -0,0 +1,9 @@
+import sqlite3 as sql
+import numpy as np
+
+conn = sql.connect("../data/database.sqlite")
+c = conn.cursor()
+
+print c.execute("select * from county_facts").fetchone()
+
+conn.close()
